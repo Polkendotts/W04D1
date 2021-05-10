8f08ad1586fcbe33f00bc3fa2d05e3e78318a48a
Lori <erica.iida14@gmail.com>
2021-05-10 13:53:08 -0400

    phaseI- tictactoenode

--- tic_tac_toe/lib/tic_tac_toe_node.rb
+++ tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -7,18 +7,12 @@ class TicTacToeNode
     @prev_mov_pos = @prev_mov_pos
   end
 
-  def children
-    @board.each_with_index do |row, i|
-      row.each_index do |j|
-        if @board.empty?([i,j])
-          new_board = @board.dup
-          TicTacToeNode.new()
-        end
-      end
-    end
-  end
-
   def losing_node?(evaluator)
+    return @board.winner != evaluator if @board.over? 
+    if evaluator == @next_mover_mark 
+
+
+
 
   end
 
@@ -29,6 +23,26 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    next_moves = []
 
+    @board.each_with_index do |row, i|
+      row.each_index do |j|
+        if @board.empty?([i,j])
+          new_board = @board.dup 
+          new_board[i,j] = @next_mover_mark
+          next_moves << TicTacToeNode.new(new_board, swap_mark(@next_mover_mark), [i,j])
+        end
+      end
+    end
+
+    next_moves
   end
+
+  def swap_mark(mark)
+    mark == :x ? :o : :x
+  end 
+
+
+
+
 end
