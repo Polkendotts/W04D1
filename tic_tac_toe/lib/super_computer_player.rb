263d524459361e3e65d3d1f671b1023d54e4bf52
Lori <erica.iida14@gmail.com>
2021-05-10 15:38:31 -0400

    supercomputer class created

--- tic_tac_toe/lib/super_computer_player.rb
+++ tic_tac_toe/lib/super_computer_player.rb
@@ -2,6 +2,10 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    board_node = TicTacToeNode.new(game.board, mark)
+    board_node.children.each {|child| return child.prev_move_pos if child.winning_node?(mark)}
+    board_node.children.each {|child| return child.prev_move_pos unless child.losing_node?(mark) }
+    raise "Impossible Game!"
   end
 end
 
